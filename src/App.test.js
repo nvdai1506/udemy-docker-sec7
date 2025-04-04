f81c8c6a1338d0f7f18493897880d15e40f273f7
vandai.nguyen <vandai.nguyen@mail.finviet.com.vn>
2025-04-04 09:08:15 +0700

    remove test fail case

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,3 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
-
-test('failed', () => {
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).not.toBeInTheDocument();
-});
